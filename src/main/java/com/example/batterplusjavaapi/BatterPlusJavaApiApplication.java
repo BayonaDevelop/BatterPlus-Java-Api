567ad5e391dbbfdb0602f5d7fe1c79fec7369eaa
BayonaDevelop <104669747+BayonaDevelop@users.noreply.github.com>
2023-04-20 15:22:16 -0700

    Initial commit

---
+++ src/main/java/com/example/batterplusjavaapi/BatterPlusJavaApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.batterplusjavaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatterPlusJavaApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BatterPlusJavaApiApplication.class, args);
+  }
+
+}
